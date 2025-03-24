58cf866dd2192e1512839767666e9b97d1a70847
Dmitriy <d.klyukanov@gmail.com>
2025-03-24 20:49:39 +0500

    Homework CRUD в DRF

--- 3.2/library/main/serializers.py
+++ 3.2/library/main/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
+from main.models import Book, Order
+
 
 class BookSerializer(serializers.ModelSerializer):
     # реализуйте сериализацию объектов модели Book
-    ...
+    class Meta:
+        model = Book
+        fields = ['author','title','year']
 
     #доп задание
     # def to_representation(self, instance):
@@ -14,7 +18,9 @@ class BookSerializer(serializers.ModelSerializer):
 
 class OrderSerializer(serializers.ModelSerializer):
     # добавьте поля модели Order
-    ...
+    class Meta:
+        model = Order
+        fields = ['user_name', 'days_count', 'date', 'books']
 
     #доп задание
     # def to_representation(self, instance):
